ede66e1d5e01319cc3eec769470e60cf23f5ad50
NAVAJAVA <nshemoul@gmail.com>
2017-04-02 13:16:49 +0300

    added fragment to navi - bar, calendar works

--- app/src/main/java/com/example/nava/a2003/OperationActivity.java
+++ app/src/main/java/com/example/nava/a2003/OperationActivity.java
@@ -1,8 +1,12 @@
 package com.example.nava.a2003;
 
+import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +17,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class OperationActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class OperationActivity extends  AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentOne.OnFragmentInteractionListener,
+        FragmentTwo.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,21 @@ public class OperationActivity extends AppCompatActivity
         setContentView(R.layout.activity_operation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (savedInstanceState == null) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = FragmentOne.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        }
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -79,12 +99,16 @@ public class OperationActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
         if (id == R.id.nav_calendar) {
+            fragmentClass = FragmentTwo.class;
 
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_rsvp) {
+
 
         } else if (id == R.id.nav_manage) {
 
@@ -95,8 +119,21 @@ public class OperationActivity extends AppCompatActivity
 
         }
 
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
